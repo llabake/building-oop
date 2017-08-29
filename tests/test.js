892bafa8e19b1c1274dc4874105192ea5d65ae0e
llabake <lemboyelabake@gmail.com>
2017-08-29 18:27:17 +0100

    add two extra test cases

--- tests/test.js
+++ tests/test.js
@@ -4,15 +4,26 @@ import { Building, ResidentialBuilding, CommercialBuilding, } from '../src/main'
 import chai  from 'chai';
 const assert = chai.assert;
 
-describe("Building Class", function() {
-    
+describe("Building Class:", function() {
+    let dangote = new Building({location:'Dangote',value:2000000, width:675, height:976});
+    let otedola = new ResidentialBuilding({location:'Otedola',value:5000000, width:975, height:776, noOfApartments: 5, noOfOccupants: 4})
     it("The building should be a type of `object`, and an instance of the `building` class", () => {
-        let dangote = new Building('Dangote,');
+        // let dangote = new Building('Dangote,');
         assert.equal(typeof {},typeof dangote);
         assert.isTrue(true, dangote instanceof Building );
         
         // expect(dangote instanceof Building).toBeTruthy();
     });
+
+    it(`${dangote.height} X ${dangote.width}`, () => {
+        assert.equal(dangote.getDimension(),(`${dangote.height} X ${dangote.width}`));
+    });
+
+    it(`${otedola.height} X ${otedola.width}`, () => {
+        assert.equal(otedola.getDimension(),(`${otedola.height} X ${otedola.width}`));
+    });
+
+
 
 
     // it(`${this.height} X ${this.width}`, () => {
